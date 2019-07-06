1e3b2711c17585b817d18919dfa90d5cf32c0094
TamarAsashvili <asashvili@gmail.com>
2019-07-06 20:54:47 +0200

    search photos

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AppBar from "material-ui/AppBar";
+
+const NavBar = () => (
+  <AppBar title="PixaBay Image Finder" style={{ backgroundColor: "pink" }} />
+);
+
+export default NavBar;
